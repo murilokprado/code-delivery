f7d43d8c6947218b2b9fd0de9cd4910a0b5df5f4
Murilo Prado <mrlkprado@gmail.com>
2023-02-09 22:52:52 -0300

    second commit

---
+++ nest-api/src/routes/routes.controller.ts
@@ -0,0 +1,85 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { RoutesService } from './routes.service';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+import { ClientKafka } from '@nestjs/microservices/client';
+import { Inject } from '@nestjs/common/decorators';
+import { OnModuleInit } from '@nestjs/common/interfaces';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
+import { MessagePattern, Payload } from '@nestjs/microservices/decorators';
+
+@Controller('routes')
+export class RoutesController implements OnModuleInit {
+  private kafkaProducer: Producer;
+
+  constructor(
+    private readonly routesService: RoutesService,
+    @Inject('KAFKA_SERVICE')
+    private kafkaCliente: ClientKafka,
+  ) {}
+
+  @Post()
+  create(@Body() createRouteDto: CreateRouteDto) {
+    return this.routesService.create(createRouteDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.routesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.routesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateRouteDto: UpdateRouteDto) {
+    return this.routesService.update(+id, updateRouteDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.routesService.remove(+id);
+  }
+
+  async onModuleInit() {
+    this.kafkaProducer = await this.kafkaCliente.connect();
+  }
+
+  @Get(':id/start')
+  startRoute(@Param('id') id: string) {
+    this.kafkaProducer.send({
+      topic: 'route.new-direction',
+      messages: [
+        {
+          key: 'route.new-direction',
+          value: JSON.stringify({ routeId: id, clientId: '' }),
+        },
+      ],
+    });
+  }
+
+  @MessagePattern('route.new-position')
+  consumeNewPosition(
+    @Payload()
+    message: {
+      value: {
+        routeId: string;
+        clientId: string;
+        position: [number, number];
+        finished: boolean;
+      };
+    },
+  ) {
+    console.log(message.value);
+  }
+}
